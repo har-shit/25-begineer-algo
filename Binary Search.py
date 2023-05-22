a7449eda08e552c6019f1b554eeb5c3f19862bdb
har-shit <hkchhwa@gmail.com>
2023-05-23 00:06:46 +0530

    2nd one

---
+++ Binary Search.py
@@ -0,0 +1,8 @@
+arr = [1, 3, 8, 9, 13, 21, 25, 27, 28, 32, 38, 44, 48, 49, 56, 59, 61, 62, 66, 77, 79, 83, 86, 88, 94, 99]
+
+length = len(arr)
+
+desired_value = int(input("Value to find : "))
+
+def bin_ser(Value, list, Length):
+    if 
