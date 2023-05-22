139458046e3bba7aec76571e41a64ad0d12ee8c5
har-shit <hkchhwa@gmail.com>
2023-05-22 20:41:21 +0530

    adds comments

--- linear Search.py
+++ linear Search.py
@@ -1,7 +1,11 @@
+# list of random elements
 arr = [4, 65, 98, 34, 98, 21, 98, 9, 95, 27, 8,  7, 65, 4, 9 ,654, 89, 29, 83, 78, 32, 19, 79, 23, 64, 32]
 
+#taking input value to find in list
 value_to_find = int(input("Value to find :"))
 
+#this loop covers all elements in the list until it finds the desired value to find and prints -
+# the position of the value in list
 for i in arr:
     if i == value_to_find:
         if (arr.index(i)+1)==1:
