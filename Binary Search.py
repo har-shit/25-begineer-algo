83a2e555f47c963e549a4f19f49bf8dbd8faf444
har-shit <hkchhwa@gmail.com>
2023-07-23 15:16:41 +0530

    auto selecting random element

--- Binary Search.py
+++ Binary Search.py
@@ -1,10 +1,19 @@
 #Creating a list with 1000 random elements
-from random import randint
+from random import randint, choice
 arr = []
 for i in range(100):
     x = randint(-100, 100)
     arr.append(x)
 
+#selecting a random element from the digit to search for. 
+vf = choice(arr)
+
+#sorting the list to perform binary search
+arr.sort()
+
+#printing the list and the element to find after sorting the list
+print(arr)
+print(f"Value to find is {vf}")
 
         
 #Defining the Binary Search algorithm
@@ -23,14 +32,11 @@ def BS(arr, vf):
         else:
             H = mid-1
     if found == True:
-        print(f"Desired Value {vf} Found on index {arr.index(vf)}")
+        print(f"Desired Value {vf} Found on index {mid}")
     else:
         print("Not Found") 
 
 
 
-   
-#Driver code
-arr.sort()
-vf = int(input("Input The Value To Find in the random list :"))
+#envoking the binary search function
 BS(arr, vf)
